712d9d9df50dd5206f7038ec8c229cdfdda9b2c8
The Nithin Balaji <73932121+thenithinbalaji@users.noreply.github.com>
2021-06-03 22:48:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+import requests
+import json
+from alive import keep_alive
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('rn hello'):
+    await message.channel.send("Hello Dude")
+   
+  elif msg.startswith('rn bye'):
+    await message.channel.send("Bub Bye! Happy to see you go LOL")
+
+  
+keep_alive()
+client.run(os.getenv('YOUR BOT TOKEN'))
