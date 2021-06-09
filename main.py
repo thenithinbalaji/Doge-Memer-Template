46c4bbfd96fc00d4aac61936dff61e76700d36bd
The Nithin Balaji <73932121+thenithinbalaji@users.noreply.github.com>
2021-06-09 17:32:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('Bot is running')
 
 @client.event
 async def on_message(message):
